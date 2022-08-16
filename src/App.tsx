f738780edd2e56d3a6e385c1fcdccbd7153f91b2
Roman Troshin <roman.troshin98@gmail.com>
2022-08-17 00:47:01 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
@@ -6,6 +7,16 @@ function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<p>Home</p>} />
+        <Route path="/book" element={<p>Book</p>} />
+        <Route path="/games" element={<p>Games</p>} />
+        <Route path="/games/sprint" element={<p>Sprint</p>} />
+        <Route path="/games/audio" element={<p>Audio</p>} />
+        <Route path="/statistics" element={<p>Statistics</p>} />
+        <Route path="/about-project" element={<p>About Project</p>} />
+        <Route path="/about-team" element={<p>About Team</p>} />
+      </Routes>
       <div>RsLang</div>
       <Footer />
     </>
