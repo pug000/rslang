6a900dd24b0accc793dd6cfd875ab7fb42c72b65
Anastasiya Sachko <naztya12323@gmail.com>
2022-08-17 02:50:42 +0300

    refactor: add all styles imports to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import Global from '@/styles/Global';
+import defaultTheme from '@/styles/theme';
+
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <Global />
       <Header />
       <div>RsLang</div>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
