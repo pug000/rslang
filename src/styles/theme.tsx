744b9afcba1171bb769c2eb3edf1148c000ae48a
Roman Troshin <roman.troshin98@gmail.com>
2022-08-17 17:13:01 +0300

    refactor: change default theme interface

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,40 @@
-import { Theme } from '@/ts/interfaces';
+import { DefaultTheme } from 'styled-components';
 
-const defaultTheme: Theme = {
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      title: string,
+      text: string,
+      textBold: string,
+      textHighlighted: string,
+      textButton: string,
+      bgWhite: string,
+      bgBeige: string,
+      bgPink: string,
+      bgPurple: string,
+      beige: string,
+      blue: string,
+      grey: string,
+      orange: string,
+      pink: string,
+      purple: string,
+    }
+    fonts: {
+      title: string,
+      text: string,
+    }
+    fontSizes: {
+      h1: string,
+      h2: string,
+      h3: string,
+      h4: string,
+      h5: string,
+      text: string,
+    }
+  }
+}
+
+const defaultTheme: DefaultTheme = {
   colors: {
     title: '#222222',
     text: '#404756',
