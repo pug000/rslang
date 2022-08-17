8d355fd467bff2df161054b68ed0f94951594120
Roman Troshin <roman.troshin98@gmail.com>
2022-08-17 14:16:34 +0300

    feat: add default fonts

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import defaultTheme from './theme';
 
 const Global = createGlobalStyle`
 *,
@@ -7,14 +8,26 @@ const Global = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
+  font-weight: 400;
 }
 
 body {
-  background-color: #ffffff;
+  background-color: ${defaultTheme.colors.bgWhite};
+  font-family: ${defaultTheme.fonts.text};
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 700;
+  font-family: ${defaultTheme.fonts.title};
 }
 
 #root {
-  color: #404756;
+  color: ${defaultTheme.colors.grey};
 }
 `;
 
