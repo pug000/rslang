e4740342e28e4fe0b6193ebca9c32a54af20d378
Roman Troshin <roman.troshin98@gmail.com>
2022-08-17 14:25:37 +0300

    refactor: replace the theme interface to a file with interfaces

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,36 +1,4 @@
-import { DefaultTheme } from 'styled-components';
-
-interface Theme extends DefaultTheme {
-  colors: {
-    title: string,
-    text: string,
-    textBold: string,
-    textHighlighted: string,
-    textButton: string,
-    bgWhite: string,
-    bgBeige: string,
-    bgPink: string,
-    bgPurple: string,
-    beige: string,
-    blue: string,
-    grey: string,
-    orange: string,
-    pink: string,
-    purple: string,
-  }
-  fonts: {
-    title: string,
-    text: string,
-  }
-  fontSizes: {
-    h1: string,
-    h2: string,
-    h3: string,
-    h4: string,
-    h5: string,
-    text: string,
-  }
-}
+import { Theme } from '@/ts/interfaces';
 
 const defaultTheme: Theme = {
   colors: {
