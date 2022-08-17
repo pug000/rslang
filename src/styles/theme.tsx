fd84805c2cd54b4c8ccba7191954835e275c69e4
Roman Troshin <roman.troshin98@gmail.com>
2022-08-17 11:36:27 +0300

    fix: update interface for default theme object

--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,6 +1,38 @@
 import { DefaultTheme } from 'styled-components';
 
-const defaultTheme: DefaultTheme = {
+interface Theme extends DefaultTheme {
+  colors: {
+    title: string,
+    text: string,
+    textBold: string,
+    textHighlighted: string,
+    textButton: string,
+    bgWhite: string,
+    bgBeige: string,
+    bgPink: string,
+    bgPurple: string,
+    beige: string,
+    blue: string,
+    grey: string,
+    orange: string,
+    pink: string,
+    purple: string,
+  }
+  fonts: {
+    title: string,
+    text: string,
+  }
+  fontSizes: {
+    h1: string,
+    h2: string,
+    h3: string,
+    h4: string,
+    h5: string,
+    text: string,
+  }
+}
+
+const defaultTheme: Theme = {
   colors: {
     title: '#222222',
     text: '#404756',
