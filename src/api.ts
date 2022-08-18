96bcaf6536e684e1e962506ff3d309e685d4e0ef
Roman Troshin <roman.troshin98@gmail.com>
2022-08-18 21:20:26 +0300

    feat: add base api

---
+++ src/api.ts
@@ -0,0 +1,18 @@
+const baseUrl = 'https://react-learnwords-example.herokuapp.com';
+
+const endpoints = {
+  words: 'words',
+  users: 'users',
+  signin: 'signin',
+  tokens: 'tokens',
+  aggreagatedWords: 'aggregatedWords',
+  statistics: 'statistics',
+  settings: 'settings'
+};
+
+const methods = {
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  delete: 'DELETE',
+};
