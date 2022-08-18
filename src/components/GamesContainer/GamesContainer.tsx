42576ac2dc8e75d56212d96145a1b91281d94ecb
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-08-18 19:23:30 +0300

    feat: move style to file

--- src/components/GamesContainer/GamesContainer.tsx
+++ src/components/GamesContainer/GamesContainer.tsx
@@ -1,57 +1,27 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom';
-import styled from 'styled-components';
+import React from 'react';
 import GameElement from './GameElement';
+import {
+  GamesMainContainer, GamesTitle, GamesWrapper, LinkGame
+} from './Games.style';
 
-const GameContainer = styled.div`
-  /* min-height: 40rem;
-  max-width: 80rem; */
-  padding: 20px 25px 10px;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  border: solid 2px red;
-`
-const GameContainerTitle = styled.h2`
-  font-family: ${(props) => props.theme.fonts.title};
-  font-size: ${(props) => props.theme.fontSizes.h2};
-  font-weight: 700;
-  margin-bottom: 1.5rem;
-`
-const GamesWrapper = styled.div`
-  /* width: 80%; */
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-items: center;
-  gap: 1.5rem;
-`
-const LinkGame = styled(NavLink)`
-  &:visited, &:link, &:active, &:hover, &:focus{
-    text-decoration: none;
-  }
-`
-
-const GamesContainer = () => {
+function GamesContainer() {
   return (
-    <GameContainer>
-      <GameContainerTitle>Игры</GameContainerTitle>
+    <GamesMainContainer>
+      <GamesTitle>Игры</GamesTitle>
       <GamesWrapper>
         <LinkGame to="/sprint">
           <GameElement
-            title='Спринт'
+            title="Спринт"
           />
         </LinkGame>
-        <LinkGame to="/audio" >
+        <LinkGame to="/audio">
           <GameElement
-            title='Аудиовызов'
+            title="Аудиовызов"
           />
         </LinkGame>
       </GamesWrapper>
-    </GameContainer>
-  )
+    </GamesMainContainer>
+  );
 }
 
-export default GamesContainer
+export default GamesContainer;
