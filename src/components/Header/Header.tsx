6ad9d469a041f8eebbfe2481ed38fae8d9c31a25
Roman Troshin <95237510+pug000@users.noreply.github.com>
2022-08-18 17:27:36 +0300

    fix: merge pull request "Create UI for menu"
    * feat: render nav menu
    
    * feat: add animation for nav menu
    
    * feat: add hover effect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
+import NavMenu from '@/NavMenu';
 import {
   HeaderContainer, HeaderBtn, HeaderLogoTitle, iconStyles
 } from './styled';
 
 function Header() {
+  const [isNavMenuOpen, setNavMenuOpen] = useState(false);
+
   return (
     <HeaderContainer>
-      <HeaderBtn>
+      <HeaderBtn onClick={() => setNavMenuOpen(!isNavMenuOpen)}>
         <MenuIcon sx={iconStyles} />
       </HeaderBtn>
       <HeaderLogoTitle>RSLang</HeaderLogoTitle>
       <HeaderBtn>
         <LoginIcon sx={iconStyles} />
       </HeaderBtn>
+      <NavMenu isNavMenuOpen={isNavMenuOpen} />
     </HeaderContainer>
   );
 }
---
+++ src/ts/interfaces.ts
@@ -0,0 +1,5 @@
+export interface NavItem {
+  id: number,
+  value: string,
+  link: string,
+}
