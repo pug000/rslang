b776d35fb6af59d9c4e0261f8db21ac3a0e79229
Anastasiya Sachko <95384801+saachko@users.noreply.github.com>
2022-08-19 20:15:35 +0300

    fixL merge pull request 'Create UI for sign in'
    * refactor: add path to SignIn element
    
    * feat: create a modal window for future signIn component
    
    * refactor: add eslint-disable to a line with warning
    
    * fix: add a prop for selecting button type
    
    * fix: fix components in order to increase usability
    
    * fest: add UI for sign in/sign up
    
    * refactor: add icon for sign in
    
    * feat: add global state isSignedIn
    
    * refactor: add inputs and buttons to a form tag
    
    * fix: remove className
    
    * feat: add hover effect to a LoginBtn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,37 @@
 import React, { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import LoginIcon from '@mui/icons-material/Login';
-// import LogoutIcon from '@mui/icons-material/Logout'; //раскомментируйте, когда понадобится
+// import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 import NavMenu from '@/NavMenu';
+import SignInModal from '@/SignIn';
 import {
-  HeaderContainer, HeaderBtn, iconStyles, HeaderLink
+  HeaderContainer, HeaderBtn, iconStyles, HeaderLink, LoginBtn
 } from './styled';
 import LogoSvg from './LogoSvg';
 
 function Header() {
   const [isNavMenuOpen, setNavMenuOpen] = useState(false);
+  const [modalActive, setModalActive] = useState(false);
+  const [isSignedIn, setSignIn] = useState(true);
 
   return (
-    <HeaderContainer>
-      <HeaderBtn onClick={() => setNavMenuOpen(!isNavMenuOpen)}>
-        <MenuIcon sx={iconStyles} />
-      </HeaderBtn>
-      <HeaderLink to="/"><LogoSvg /></HeaderLink>
-      <HeaderBtn>
-        <LoginIcon sx={iconStyles} />
-      </HeaderBtn>
-      <NavMenu isNavMenuOpen={isNavMenuOpen} />
-    </HeaderContainer>
+    <>
+      <HeaderContainer>
+        <HeaderBtn onClick={() => setNavMenuOpen(modalActive ? false : !isNavMenuOpen)}>
+          <MenuIcon sx={iconStyles} />
+        </HeaderBtn>
+        <HeaderLink to="/"><LogoSvg /></HeaderLink>
+        <HeaderBtn>
+          {isSignedIn
+            ? <LogoutIcon sx={iconStyles} onClick={() => setSignIn(false)} />
+            : (
+              <LoginBtn onClick={() => setModalActive(!modalActive)} />)}
+        </HeaderBtn>
+        <NavMenu isNavMenuOpen={isNavMenuOpen} />
+      </HeaderContainer>
+
+      <SignInModal active={modalActive} setActive={setModalActive} />
+    </>
   );
 }
 
---
+++ src/components/Header/SignIn/SignIn.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Input from '@/Input';
+import Button from '@/Button';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+  Shadow, Modal, SignInTitle, CloseBtn, iconStyles, shadowActive, modalActive
+} from './SignIn.style';
+
+interface SignInProps {
+  active: boolean;
+  setActive: (arg0: boolean) => void;
+}
+
+function SignInModal({ active, setActive }: SignInProps) {
+  return (
+    <Shadow onClick={() => setActive(false)} style={active ? shadowActive : undefined}>
+      <Modal onClick={(e) => e.stopPropagation()} style={active ? modalActive : undefined}>
+        <CloseBtn onClick={() => setActive(false)}>
+          <CloseIcon sx={iconStyles} />
+        </CloseBtn>
+        <SignInTitle>Добро пожаловать!</SignInTitle>
+        <form>
+          <Input type="email" title="" id="login" placeholder="Введите Ваш e-mail" name="login" />
+          <Input type="password" title="" id="pass" placeholder="Введите Ваш пароль" name="pass" />
+          <Button id="signIn" title="Войти" callback={() => { }} />
+          <Button id="signUp" title="Зарегистрироваться" callback={() => { }} />
+        </form>
+      </Modal>
+    </Shadow>
+  );
+}
+
+export default SignInModal;
