9b6b857d907535fac73213358ccef305fcf8c886
Anastasiya Sachko <naztya12323@gmail.com>
2022-08-21 03:31:59 +0300

    feat: create UI for Home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import Button from '@/Button';
+import { Link } from 'react-router-dom';
+import {
+  HomeContainer, Bg, BgLeft, BgRight, HomeTextContainer, HomeTitle, HomeText, HomeBtns
+} from './Home.style';
+import { BgLeftIcon, BgRightIcon } from './HomeBg';
 
 function Home() {
   return (
-
+    <HomeContainer>
+      <Bg>
+        <BgLeft>
+          <BgLeftIcon />
+        </BgLeft>
+        <BgRight>
+          <BgRightIcon />
+        </BgRight>
+      </Bg>
+      <HomeTextContainer>
+        <HomeTitle>Английский - это легко!</HomeTitle>
+        <HomeText>
+          RS Lang — это эффективный сервис для увлекательной практики английского языка.
+          Учиться здесь весело и интересно.
+        </HomeText>
+        <HomeBtns>
+          <Link to="/book">
+            <Button id="toBook" title="Учебник" callback={() => { }} />
+          </Link>
+          <Link to="/games">
+            <Button id="toGames" title="Игры" callback={() => { }} />
+          </Link>
+        </HomeBtns>
+      </HomeTextContainer>
+    </HomeContainer>
   );
 }
 
