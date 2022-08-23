411e76968e681625c0c1893e5265f05ae50db45c
Roman Troshin <roman.troshin98@gmail.com>
2022-08-24 00:20:35 +0300

    fix: paths on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React, { useEffect, useMemo, useState } from 'react';
 
-import { Route, Routes, Navigate } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-
-import Global from '@/styles/Global';
-import defaultTheme from '@/styles/theme';
-
-import Header from '@/Header';
-import Footer from '@/Footer';
+import { Route, Routes } from 'react-router-dom';
 import Book from '@/Book';
 import GameContainer from '@/GamesContainer';
 import { WordData } from '@/ts/interfaces';
 import WordItemContext from '@/contexts/WordItemContext';
 import ProtectedRoute from '@/ProtectedRoute';
 import DifficultWords from '@/DifficultWords';
+import AppLayout from '@/AppLayout';
 import useLocalStorage from './hooks/useLocalStorage';
 import Home from './components/Home/Home';
 
@@ -38,50 +32,48 @@ function App() {
   ), [difficultWords, learnedWords]);
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Global />
-      <Header
-        isLoggedIn={isLoggedIn}
-        setIsLoggedIn={setIsLoggedIn}
-      />
-      <main>
-        <Routes>
-          <Route path="dist/index.html" element={<Navigate replace to="/" />} />
-          <Route path="/index.html" element={<Navigate replace to="/" />} />
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/book"
-            element={(
-              <WordItemContext.Provider value={wordItemValue}>
-                <Book />
-              </WordItemContext.Provider>
-            )}
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <AppLayout
+            isLoggedIn={isLoggedIn}
+            setIsLoggedIn={setIsLoggedIn}
           />
-          <Route
-            path="/difficult-words"
-            element={(
-              <ProtectedRoute conditionValue={isLoggedIn}>
-                <DifficultWords isLoggedIn={isLoggedIn} />
-              </ProtectedRoute>
-            )}
-          />
-          <Route path="/games" element={<GameContainer />} />
-          <Route path="/games/sprint" element={<p>Sprint</p>} />
-          <Route path="/games/audio" element={<p>Audio</p>} />
-          <Route
-            path="/statistics"
-            element={(
-              <ProtectedRoute conditionValue={isLoggedIn}>
-                <p>Statistics</p>
-              </ProtectedRoute>
-            )}
-          />
-          <Route path="/about-project" element={<p>About Project</p>} />
-          <Route path="/about-team" element={<p>About Team</p>} />
-        </Routes>
-      </main>
-      <Footer />
-    </ThemeProvider>
+        )}
+      >
+        <Route index element={<Home />} />
+        <Route
+          path="book"
+          element={(
+            <WordItemContext.Provider value={wordItemValue}>
+              <Book />
+            </WordItemContext.Provider>
+          )}
+        />
+        <Route
+          path="difficult-words"
+          element={(
+            <ProtectedRoute conditionValue={isLoggedIn}>
+              <DifficultWords isLoggedIn={isLoggedIn} />
+            </ProtectedRoute>
+          )}
+        />
+        <Route path="games" element={<GameContainer />} />
+        <Route path="games/sprint" element={<p>Sprint</p>} />
+        <Route path="games/audio" element={<p>Audio</p>} />
+        <Route
+          path="statistics"
+          element={(
+            <ProtectedRoute conditionValue={isLoggedIn}>
+              <p>Statistics</p>
+            </ProtectedRoute>
+          )}
+        />
+        <Route path="about-project" element={<p>About Project</p>} />
+        <Route path="about-team" element={<p>About Team</p>} />
+      </Route>
+    </Routes>
   );
 }
 
