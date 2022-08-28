8da402322b16440fd13900982bc53b596d459567
aArt-13 <kharytonchyk.artsiom@gmail.com>
2022-08-28 13:21:08 +0300

    fix: eslint fix

--- src/components/Header/SignIn/SignIn.tsx
+++ src/components/Header/SignIn/SignIn.tsx
@@ -53,7 +53,7 @@ function SignInModal({
   const dafaultErrMessage = {
     text: '',
     activeErr: false,
-  }
+  };
 
   const [errMessage, setErrMessage] = useState<ErrMessageProps>(dafaultErrMessage);
 
@@ -61,7 +61,7 @@ function SignInModal({
   const changeWaitingData = () => setIsWaitingData(((prev) => !prev));
   const changeActiveShadow = () => setActive(false);
   const errMessageShow = (text: string, activeErr: boolean) => {
-    setErrMessage({ ...errMessage, text: text, activeErr: activeErr });
+    setErrMessage({ ...errMessage, text, activeErr });
     changeErrShow();
     setTimeout(changeErrShow, 3000);
   };
