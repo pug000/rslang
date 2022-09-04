b2972fb53887117f77dd116533c7df2eeb94db14
Anastasiya Sachko <naztya12323@gmail.com>
2022-09-04 13:31:43 +0300

    fix: fix statistics styles and add loader

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,5 +1,7 @@
-/* eslint-disable react/no-unstable-nested-components */
-import React, { useEffect, useState } from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react';
 import {
   BarChart,
   CartesianGrid,
@@ -19,13 +21,21 @@ import {
 
 import defaultTheme from '@/styles/theme';
 
+import Loader from '@/Loader';
+
 import {
   StatisticsTitle,
   GameCharts,
   TotalCharts,
   TotalChartsText,
-  HighlightedText
+  HighlightedText,
+  SvgWrapper,
+  NoStatisticsWrapper,
+  LoaderWrapper,
+  NoStatisticsTitle,
+  Note
 } from './Statistics.style';
+import BackgroundElement from './StatisticsSvg';
 
 interface StatisticsProps {
   isLoggedIn: boolean,
@@ -38,18 +48,23 @@ function Statistics({
   token,
   userId
 }: StatisticsProps) {
+  const [isStatisticsShown, setIsStatisticsShown] = useState(false);
+  const [isLoadingPage, setIsLoadingPage] = useState(false);
   const [date, setDate] = useState<string | null>(null);
   const [dataGameLearnedWords, setDataGameLearnedWords] = useState<DataForCharts[]>([]);
   const [dataPercentCorrectAnswers, setDataPercentCorrectAnswers] = useState<DataForCharts[]>([]);
   const [dataMaxCountCorrectAnswers, setDataMaxCountCorrectAnswers] = useState<DataForCharts[]>([]);
   const [dataTotalStatistics, setDataTotalStatistics] = useState<DataForTotalStatistics[]>([]);
   const [totalCorrectPercent, setTotalCorrectPercent] = useState('');
+  const coloredLegend = (value: string) => <span className="coloredText">{value}</span>;
 
-  if (!isLoggedIn) return null;
   useEffect(() => {
     if (isLoggedIn) {
       (async () => {
+        setIsLoadingPage(true);
         const data = await getUserStatistics(userId, token);
+        if (data.optional.games.sprint.gameLearnedWords
+          || data.optional.games.audio.gameLearnedWords) setIsStatisticsShown(true);
         setDate(data.optional.date);
         setDataGameLearnedWords([{
           name: 'Новые слова за день',
@@ -78,103 +93,125 @@ function Statistics({
 
         const correctPercent = ((totalCorrectWords / totalWords) * 100).toString().substring(0, 4);
         setTotalCorrectPercent(correctPercent);
+        setIsLoadingPage(false);
       })();
     }
   }, []);
 
+  if (isStatisticsShown) {
+    return (
+      <>
+        <StatisticsTitle>
+          {'Дневная статистика на '}
+          {date}
+        </StatisticsTitle>
+        <GameCharts>
+          <BarChart width={310} height={300} data={dataGameLearnedWords}>
+            <CartesianGrid strokeDasharray="1 1" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend formatter={coloredLegend} />
+            <Bar
+              name="Спринт"
+              dataKey="sprint"
+              fill={defaultTheme.colors.backgroundBlue}
+              stroke={defaultTheme.colors.text}
+            />
+            <Bar
+              name="Аудиовызов"
+              dataKey="audio"
+              fill={defaultTheme.colors.backgroundPink}
+              stroke={defaultTheme.colors.text}
+            />
+          </BarChart>
+          <BarChart width={310} height={300} data={dataPercentCorrectAnswers}>
+            <CartesianGrid strokeDasharray="1 1" />
+            <XAxis dataKey="name" />
+            <YAxis type="number" domain={[0, 100]} />
+            <Tooltip />
+            <Legend formatter={coloredLegend} />
+            <Bar
+              name="Спринт"
+              dataKey="sprint"
+              fill={defaultTheme.colors.backgroundBlue}
+              stroke={defaultTheme.colors.text}
+            />
+            <Bar
+              name="Аудиовызов"
+              dataKey="audio"
+              fill={defaultTheme.colors.backgroundPink}
+              stroke={defaultTheme.colors.text}
+            />
+          </BarChart>
+          <BarChart width={310} height={300} data={dataMaxCountCorrectAnswers}>
+            <CartesianGrid strokeDasharray="1 1" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend formatter={coloredLegend} />
+            <Bar
+              name="Спринт"
+              dataKey="sprint"
+              fill={defaultTheme.colors.backgroundBlue}
+              stroke={defaultTheme.colors.text}
+            />
+            <Bar
+              name="Аудиовызов"
+              dataKey="audio"
+              fill={defaultTheme.colors.backgroundPink}
+              stroke={defaultTheme.colors.text}
+            />
+          </BarChart>
+        </GameCharts>
+        <StatisticsTitle>Общая дневная статистика</StatisticsTitle>
+        <TotalCharts>
+          <BarChart width={310} height={300} data={dataTotalStatistics}>
+            <CartesianGrid strokeDasharray="1 1" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend formatter={coloredLegend} />
+            <Bar
+              name="Всего новых слов"
+              dataKey="total"
+              fill={defaultTheme.colors.backgroundPurple}
+              stroke={defaultTheme.colors.text}
+            />
+            <Bar
+              name="Всего правильных ответов"
+              dataKey="correct"
+              fill={defaultTheme.colors.primaryColor}
+              stroke={defaultTheme.colors.text}
+            />
+          </BarChart>
+          <TotalChartsText>
+            Общая дневная статистика показывает Ваш результат в целом по двум играм.
+            Сегодня процент правильных ответов составил
+            <HighlightedText>{` ${totalCorrectPercent} %`}</HighlightedText>
+          </TotalChartsText>
+          <SvgWrapper>
+            <BackgroundElement />
+          </SvgWrapper>
+        </TotalCharts>
+      </>
+    );
+  }
   return (
-    <>
-      <StatisticsTitle>
-        {'Дневная статистика на '}
-        {date}
-      </StatisticsTitle>
-      <GameCharts>
-        <BarChart width={310} height={300} data={dataGameLearnedWords}>
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend formatter={(value) => <span className="coloredText">{value}</span>} />
-          <Bar
-            name="Спринт"
-            dataKey="sprint"
-            fill={defaultTheme.colors.backgroundBlue}
-            stroke={defaultTheme.colors.text}
-          />
-          <Bar
-            name="Аудиовызов"
-            dataKey="audio"
-            fill={defaultTheme.colors.backgroundPink}
-            stroke={defaultTheme.colors.text}
-          />
-        </BarChart>
-        <BarChart width={310} height={300} data={dataPercentCorrectAnswers}>
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="name" />
-          <YAxis type="number" domain={[0, 100]} />
-          <Tooltip />
-          <Legend />
-          <Bar
-            name="Спринт"
-            dataKey="sprint"
-            fill={defaultTheme.colors.backgroundBlue}
-            stroke={defaultTheme.colors.text}
-          />
-          <Bar
-            name="Аудиовызов"
-            dataKey="audio"
-            fill={defaultTheme.colors.backgroundPink}
-            stroke={defaultTheme.colors.text}
-          />
-        </BarChart>
-        <BarChart width={310} height={300} data={dataMaxCountCorrectAnswers}>
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar
-            name="Спринт"
-            dataKey="sprint"
-            fill={defaultTheme.colors.backgroundBlue}
-            stroke={defaultTheme.colors.text}
-          />
-          <Bar
-            name="Аудиовызов"
-            dataKey="audio"
-            fill={defaultTheme.colors.backgroundPink}
-            stroke={defaultTheme.colors.text}
-          />
-        </BarChart>
-      </GameCharts>
-      <StatisticsTitle>Общая дневная статистика</StatisticsTitle>
-      <TotalCharts>
-        <BarChart width={310} height={300} data={dataTotalStatistics}>
-          <CartesianGrid strokeDasharray="1 1" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar
-            name="Всего новых слов"
-            dataKey="total"
-            fill={defaultTheme.colors.backgroundPurple}
-            stroke={defaultTheme.colors.text}
-          />
-          <Bar
-            name="Всего правильных ответов"
-            dataKey="correct"
-            fill={defaultTheme.colors.primaryColor}
-            stroke={defaultTheme.colors.text}
-          />
-        </BarChart>
-        <TotalChartsText>
-          Общая дневная статистика показывает Ваш результат в целом по двум играм.
-          Сегодня процент правильных ответов составил
-          <HighlightedText>{` ${totalCorrectPercent} %`}</HighlightedText>
-        </TotalChartsText>
-      </TotalCharts>
-    </>
+    <NoStatisticsWrapper>
+      <NoStatisticsTitle>Дневная статистика </NoStatisticsTitle>
+      {isLoadingPage
+        ? (
+          <LoaderWrapper>
+            <Loader />
+          </LoaderWrapper>
+        )
+        : (
+          <Note>
+            Чтобы увидеть статистику, необходимо поиграть в игры.
+          </Note>
+        )}
+    </NoStatisticsWrapper>
   );
 }
 
